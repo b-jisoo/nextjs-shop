dfdbd00917ff6c5c6aadb470bd8244d3effdea48
B-jisoo <mrk334kr@naver.com>
2022-12-09 16:28:51 +0900

    Feat: 상세페이지 추가

--- client/pages/products/[...params].tsx
+++ client/pages/products/[...params].tsx
@@ -1,9 +1,57 @@
+import { useQuery } from "@apollo/client";
+import { useRouter } from "next/router";
 import React from "react";
+import { GET_PRODUCT, Product } from "../../graphql/products";
 
 type Props = {};
+type DetailParams = [string] | [];
 
 const ProductDetail = (props: Props) => {
-  return <div>ProductDetail</div>;
+  const router = useRouter();
+  const [id] = (router.query.params || []) as DetailParams;
+  if (!id) return null;
+  const { data, loading, error, refetch } = useQuery<Product>(GET_PRODUCT, {
+    variables: { id },
+  });
+  if (!data) return null;
+
+  return (
+    <section className="text-gray-700 body-font overflow-hidden bg-white">
+      <div className="container px-5 py-24 mx-auto">
+        <div className="lg:w-4/5 mx-auto flex flex-wrap">
+          <img
+            alt="ecommerce"
+            className="lg:w-1/2 w-full object-cover object-center rounded border border-gray-200"
+            src={data.product.imageUrl}
+          />
+          <div className="lg:w-1/2 w-full lg:pl-10 lg:py-6 mt-6 lg:mt-0">
+            <h2 className="text-sm title-font text-gray-500 tracking-widest">
+              {data.product.category}
+            </h2>
+            <h1 className="text-gray-900 text-3xl title-font font-medium mb-1 mt-5">
+              {data.product.title}
+            </h1>
+            <div className="flex mb-4 "></div>
+            <p className="leading-relaxed mt-10 min-h-[200px]">
+              {data.product.description}
+            </p>
+            <div className="flex mt-6 items-center pb-5 border-b-2 border-gray-200 mb-5"></div>
+            <div className="flex">
+              <span className="title-font font-medium text-2xl text-gray-900">
+                ₩{data?.product.price.toLocaleString("ko-KR")}
+              </span>
+              <button className="flex ml-auto text-white bg-red-500 border-0 py-2 px-6 focus:outline-none hover:bg-red-600 rounded">
+                Button
+              </button>
+              <button className=" bg-gray-200 py-2 px-6 border-0 inline-flex items-center justify-center text-gray-500 ml-4 rounded hover:bg-gray-300">
+                카트로 이동
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default ProductDetail;
