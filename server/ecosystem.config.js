8fb3d37cd476f18a01f099a0a27fc802e9f7e457
B-jisoo <mrk334kr@naver.com>
2022-12-29 11:32:17 +0900

    Build: pm2 설정파일 오타수정

--- server/ecosystem.config.js
+++ server/ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps: [
     {
       name: "server", // pm2 name
-      script: "ts-node ./scr/index.ts", // // 앱 실행 스크립트
+      script: "ts-node ./src/index.ts", // // 앱 실행 스크립트
       instances: 3, // 클러스터 모드 사용 시 생성할 인스턴스 수
       exec_mode: "cluster", // fork, cluster 모드 중 선택
       merge_logs: true, // 클러스터 모드 사용 시 각 클러스터에서 생성되는 로그를 한 파일로 합쳐준다.
