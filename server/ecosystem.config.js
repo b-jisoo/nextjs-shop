2c6e233a34b58083423e3a636452a013dd3b7c06
B-jisoo <mrk334kr@naver.com>
2022-12-29 11:36:15 +0900

    Build: pm2 설정 변경

--- server/ecosystem.config.js
+++ server/ecosystem.config.js
@@ -3,8 +3,8 @@ module.exports = {
     {
       name: "server", // pm2 name
       script: "ts-node ./src/index.ts", // // 앱 실행 스크립트
-      instances: 3, // 클러스터 모드 사용 시 생성할 인스턴스 수
-      exec_mode: "cluster", // fork, cluster 모드 중 선택
+      instances: 1, // 클러스터 모드 사용 시 생성할 인스턴스 수
+      exec_mode: "fork", // fork, cluster 모드 중 선택
       merge_logs: true, // 클러스터 모드 사용 시 각 클러스터에서 생성되는 로그를 한 파일로 합쳐준다.
       autorestart: true, // 프로세스 실패 시 자동으로 재시작할지 선택
       watch: false, // 파일이 변경되었을 때 재시작 할지 선택
