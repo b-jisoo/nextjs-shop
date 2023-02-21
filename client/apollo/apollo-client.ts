64a2eaef001eb871225c7468c91f63930a1ca42a
B-jisoo <mrk334kr@naver.com>
2023-02-21 19:18:52 +0900

    build: 아마존 오류 복구

--- client/apollo/apollo-client.ts
+++ client/apollo/apollo-client.ts
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 const client = new ApolloClient({
-  uri: "http://3.36.126.19:5000/graphql",
+  uri: "http://3.35.216.73:5000/graphql",
   cache: new InMemoryCache(),
 });
 
